1dcf134624874d74afdcd562d6a6e66b99ccd1bd
chunpu <527653908@qq.com>
2016-03-03 20:26:42 +0800

    m

--- browser/app.js
+++ browser/app.js
@@ -1,5 +1,6 @@
 var bestLayout = require('../')
 var _ = require('lodash')
+var Clipboard = require('clipboard')
 var debug = require('debug')('best-layout:app')
 
 $(init)
@@ -18,6 +19,7 @@ function init() {
 	var css = require('raw!./default.css')
 	console.log(css)
 
+
 	$input.on(evName, _.debounce(function() {
 		debug(evName)
 		var markdown = $input.val()
@@ -25,5 +27,26 @@ function init() {
 		var doc = template.replace('{{html}}', html).replace('{{css}}', css)
 		$iframe.attr('srcdoc', doc)
 	}, 800, {leading: true, trailing: true, maxWait: 3000})).val(demoMarkdown).trigger(evName)
+
+	
+	new Clipboard('#copy', {
+		text: function(trigger) {
+			console.log(trigger)
+			debugger
+			return '<h1>xxxx</h1>'
+		}
+	})
+
+	var $doc = $(document)
+	$doc.on('copy', function(e) {
+		var df = e.originalEvent.clipboardData
+		var selection = window.getSelection()
+		var range = selection.getRangeAt(0)
+		var fragment = range.cloneContents()
+		var html = $('<div>').append(fragment).html()
+		console.log(html)
+		df.setData('text/html', html)
+		return false
+	})
 
 }
